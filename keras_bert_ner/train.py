d57972e20886318b15d14c6d9f9892d9b2fa9daf
wzz8850563 <39791033+wzz8850563@users.noreply.github.com>
2020-04-08 22:16:53 +0800

    Update train.py

--- keras_bert_ner/train.py
+++ keras_bert_ner/train.py
@@ -27,93 +27,94 @@ from .utils.callbacks import NerCallbacks
 from .utils.metrics import CrfAcc, CrfLoss
 
 
-def train(args):
+def train(train_data,dev_data,save_path,bert_config,bert_checkpoint,bert_vocab,do_eval=1,device_map="0",tag_padding="X",best_fit=1,max_epochs=10,early_stop_patience=3, \
+reduce_lr_patience=3,reduce_lr_factor=0.5,batch_size=64,max_len=64,learning_rate=5e-6,model_type='cnn',cnn_filters=128,cnn_kernel_size=3,cnn_blocks=4,dropout_rate=0,learning_rate=5e-5):
     """模型训练流程
     """
     # 环境设置
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.device_map if args.device_map != "cpu" else ""
-    if not os.path.exists(args.save_path):
-        os.makedirs(args.save_path)
+    os.environ["CUDA_VISIBLE_DEVICES"] = device_map if device_map != "cpu" else ""
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     # 数据准备
-    processor = Processor(args.train_data, args.bert_vocab, args.tag_padding)
-    train_tokens, train_segs, train_tags = processor.process(args.train_data, args.max_len)
+    processor = Processor(train_data, bert_vocab, tag_padding)
+    train_tokens, train_segs, train_tags = processor.process(train_data, max_len)
     train_x = [np.array(train_tokens), np.array(train_segs)]
     train_y = np.array(train_tags)
-    if args.do_eval:
-        dev_tokens, dev_segs, dev_tags = processor.process(args.dev_data, args.max_len)
+    if do_eval:
+        dev_tokens, dev_segs, dev_tags = processor.process(dev_data, max_len)
         devs = [[np.array(dev_tokens), np.array(dev_segs)], np.array(dev_tags)]
     else:
         devs = None
     # 模型准备
-    if args.model_type == "cnn":
+    if model_type == "cnn":
         ner_model = NerCnnModel(
-            bert_config=args.bert_config,
-            bert_checkpoint=args.bert_checkpoint,
-            albert=args.albert,
-            max_len=args.max_len,
+            bert_config=bert_config,
+            bert_checkpoint=bert_checkpoint,
+            albert=albert,
+            max_len=max_len,
             numb_tags=processor.numb_tags,
-            dropout_rate=args.dropout_rate,
-            filters=args.cnn_filters,
-            kernel_size=args.cnn_kernel_size,
-            blocks=args.cnn_blocks).build()
-    elif args.model_type == "rnn":
+            dropout_rate=dropout_rate,
+            filters=cnn_filters,
+            kernel_size=cnn_kernel_size,
+            blocks=cnn_blocks).build()
+    elif model_type == "rnn":
         ner_model = NerRnnModel(
-            bert_config=args.bert_config,
-            bert_checkpoint=args.bert_checkpoint,
-            albert=args.albert,
-            max_len=args.max_len,
+            bert_config=bert_config,
+            bert_checkpoint=bert_checkpoint,
+            albert=albert,
+            max_len=max_len,
             numb_tags=processor.numb_tags,
-            dropout_rate=args.dropout_rate,
-            cell_type=args.cell_type,
-            units=args.rnn_units,
-            num_hidden_layers=args.rnn_num_hidden_layers).build()
+            dropout_rate=dropout_rate,
+            cell_type=cell_type,
+            units=rnn_units,
+            num_hidden_layers=rnn_num_hidden_layers).build()
     else:
         raise ValueError("model_type should be 'cnn' or 'rnn'.")
-    crf_accuracy = CrfAcc(processor.tag_to_id, args.tag_padding).crf_accuracy
-    crf_loss = CrfLoss(processor.tag_to_id, args.tag_padding).crf_loss
+    crf_accuracy = CrfAcc(processor.tag_to_id, ag_padding).crf_accuracy
+    crf_loss = CrfLoss(processor.tag_to_id, tag_padding).crf_loss
     ner_model.compile(
-        optimizer=keras.optimizers.Adam(lr=args.learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-8),
+        optimizer=keras.optimizers.Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-8),
         loss=crf_loss,
         metrics=[crf_accuracy])
     # 模型训练
-    bert_type = "ALBERT" if args.albert else "BERT"
-    model_type = "IDCNN-CRF" if args.model_type == "cnn" else ("BILSTM-CRF" if args.cell_type == "lstm" else "BIGRU-CRF")
+    bert_type = "ALBERT" if albert else "BERT"
+    model_type = "IDCNN-CRF" if model_type == "cnn" else ("BILSTM-CRF" if cell_type == "lstm" else "BIGRU-CRF")
     model_save_path = os.path.abspath(
-        os.path.join(args.save_path, "%s-%s.h5" % (bert_type, model_type)))
+        os.path.join(save_path, "%s-%s.h5" % (bert_type, model_type)))
     log_save_path = os.path.abspath(
-        os.path.join(args.save_path, "%s-%s-%s.log" % (bert_type, model_type, time.strftime("%Y%m%d_%H%M%S"))))
-    if args.best_fit:
+        os.path.join(save_path, "%s-%s-%s.log" % (bert_type, model_type, time.strftime("%Y%m%d_%H%M%S"))))
+    if best_fit:
         best_fit_params = {
-            "early_stop_patience": args.early_stop_patience,
-            "reduce_lr_patience": args.reduce_lr_patience,
-            "reduce_lr_factor": args.reduce_lr_factor,
+            "early_stop_patience": early_stop_patience,
+            "reduce_lr_patience": reduce_lr_patience,
+            "reduce_lr_factor": reduce_lr_factor,
             "save_path": model_save_path
         }
-        callbacks = NerCallbacks(processor.id_to_tag, best_fit_params, args.tag_padding, log_save_path)
-        epochs = args.max_epochs
+        callbacks = NerCallbacks(processor.id_to_tag, best_fit_params, tag_padding, log_save_path)
+        epochs = max_epochs
     else:
-        callbacks = NerCallbacks(processor.id_to_tag, None, args.tag_padding, log_save_path)
-        epochs = args.hard_epochs
+        callbacks = NerCallbacks(processor.id_to_tag, None, tag_padding, log_save_path)
+        epochs = hard_epochs
 
     ner_model.fit(
         x=train_x,
         y=train_y,
-        batch_size=args.batch_size,
+        batch_size=batch_size,
         epochs=epochs,
         callbacks=callbacks,
         validation_data=devs)
 
     # 保存信息
-    with codecs.open(os.path.join(args.save_path, "tag_to_id.pkl"), "wb") as f:
+    with codecs.open(os.path.join(save_path, "tag_to_id.pkl"), "wb") as f:
         pickle.dump(processor.tag_to_id, f)
-    with codecs.open(os.path.join(args.save_path, "id_to_tag.pkl"), "wb") as f:
+    with codecs.open(os.path.join(save_path, "id_to_tag.pkl"), "wb") as f:
         pickle.dump(processor.id_to_tag, f)
     model_configs = {
-        "max_len": args.max_len,
-        "tag_padding": args.tag_padding,
+        "max_len": max_len,
+        "tag_padding": tag_padding,
         "model_path": model_save_path,
-        "bert_vocab": os.path.abspath(args.bert_vocab)}
-    with codecs.open(os.path.join(args.save_path, "model_configs.json"), "w") as f:
+        "bert_vocab": os.path.abspath(bert_vocab)}
+    with codecs.open(os.path.join(save_path, "model_configs.json"), "w") as f:
         json.dump(model_configs, f, ensure_ascii=False, indent=4)
-    if not args.best_fit:
+    if not best_fit:
         ner_model.save(model_save_path)
